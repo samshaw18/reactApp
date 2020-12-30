b1599c5af673f5baebe08778cf412b23070563e4
thefonz122@gmail.com <thefonz122@gmail.com>
2020-12-30 18:54:50 +0200

    first commit

---
+++ src/ShoppingList.js
@@ -0,0 +1,12 @@
+import  React  from "react";
+class ShoppingList extends React.Component{
+    render() {
+        let name='dafna'
+        let age=31
+        return <div>
+            <h1>my name is {name}</h1>
+            <h3>my age is {age}</h3>
+        </div>
+    }
+}
+export default ShoppingList
